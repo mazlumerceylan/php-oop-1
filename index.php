888186947b4fb8ab392b082885cce5979da57212
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-23 23:52:41 +0200

    costruttore e metodo

--- index.php
+++ index.php
@@ -6,6 +6,20 @@ class Movie {
   public $title;
   public $director;
   public $year;
+
+
+  // Costruttore
+  public function __construct($title, $director, $year) {
+    $this->title = $title;
+    $this->director = $director;
+    $this->year = $year;
+  }
+
+  // Metodo
+  public function getMovieDetails() {
+    return "Titolo: {$this->title}, Regista: {$this->director}, Anno: {$this->year}";
+  }
 }
+
 ?>
 
